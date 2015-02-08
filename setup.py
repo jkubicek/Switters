c8e5223475cce69991b1693ce8ee95f5605c5316
Jim Kubicek <jkubicek@gmail.com>
2015-02-08 15:56:16 -0800

    Removed notes and setup script

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-setup(name='Switters',
-      version='1.0',
-      author='Jim Kubicek',
-      author_email='jkubicek@gmail.com',
-      packages=['qrcode', 'tweetpony', 'zxing', 'switterslib'],
-      )
